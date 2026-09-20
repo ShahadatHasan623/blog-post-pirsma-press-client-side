4491088e473011c264dfc1b89b775fab0821edae
ShahadatHasan623 <shahadathasan743599@gmail.com>
2026-09-20 13:09:25 +0600

    added login form functionality

--- app/(authGroup)/_components/LoginFrom.tsx
+++ app/(authGroup)/_components/LoginFrom.tsx
@@ -2,15 +2,31 @@
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
+import { loginAction } from '../_actions/actionsLogin'
+import { useActionState, useEffect } from 'react'
+import { toast } from 'sonner'
 
 
 const LoginFrom = () => {
+   const [state,action,pending]=useActionState(loginAction,false)
+
+   useEffect(()=>{
+      if(!state)return;
+      if(state.success){
+         toast.success(state.message || "Login Successful")
+      }
+      if(!state.success){
+         toast.warning(state.message || "Login Faild")
+      }
+   },[state])
   return (
-   <form className="space-y-4">
+   <form action={action} className="space-y-4">
       <Card className="p-5 space-y-1">
          <Input  name="email" type="email" placeholder="Enter Your Email" required />
          <Input name="password" type="password" placeholder="Enter Your Password" required />
-         <Button type="submit">Login</Button>
+         <Button type="submit">
+            {pending? "Submetting...":"Login"}
+         </Button>
       </Card>
    </form>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { Toaster } from "sonner";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <h1>Navbar</h1>
         {children}
+        <Toaster position="top-right" richColors  />
       </body>
     </html>
   );
